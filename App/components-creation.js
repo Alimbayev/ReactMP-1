b5ef253bd96df018111b941cbcceab34f224e106
Zarina <zhiyenbayeva.z@gmail.com>
2019-03-09 23:39:34 +0600

    First task done

---
+++ App/components-creation.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//-	React.createElement
+const divElement = React.createElement
+('div', null,
+ React.createElement('h1', null, 'Hello,')
+);
+
+ReactDOM.render(divElement, document.getElementById('app'));
+
+
+//-	React.Component
+
+const Temp = (props) => {
+    return (<div>{props.val}</div>)
+}
+
+class Welcome extends React.Component {
+    state = {
+        val: 1
+    }
+    render() {
+      return (
+      <div classname="app-2">
+        <Temp val={this.state.val}/>
+        <h1> Zarina</h1>
+      </div>
+      );
+    }
+  }
+
+  const app = document.getElementById('app-2');
+
+  ReactDOM.render(<Welcome/>, app-2);
+
+
