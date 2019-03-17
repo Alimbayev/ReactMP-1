4c2e2ced62ad4d2f3f83e96e4e9a86919cf1ecdb
Zarina <zhiyenbayeva.z@gmail.com>
2019-03-17 21:54:08 +0600

    second_task: configuring environments

--- App/index.js
+++ App/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+       console.log('Looks like we are in development mode!');
+     }
+
 app.get('/', function(req, res){
     res.send('Hello World!')
 });
