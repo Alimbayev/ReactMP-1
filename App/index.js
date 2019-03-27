c7b380abc706fe79ba9ce1cf44b9ca98e8afd97e
Zarina_Zhiyenbayeva <zhiyenbayeva.z@gmail.com>
2019-03-27 18:06:56 +0600

    Third task

--- App/index.js
+++ App/index.js
@@ -1,16 +1,6 @@
-var express = require('express');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 
-var app = express();
-
-if (process.env.NODE_ENV !== 'production') {
-       console.log('Looks like we are in development mode!');
-     }
-
-app.get('/', function(req, res){
-    res.send('Hello World!')
-});
-
-app.listen(3000, function(){
-    console.log('Server started on port 3000');
-});
+ReactDOM.render(<App />, document.getElementById('root'));
 
--- App/webpack.common.js
+++ App/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
    entry: {
-     app: './src/index.js'
+     app: './index.js'
    },
    plugins: [
      new CleanWebpackPlugin(['dist']),
