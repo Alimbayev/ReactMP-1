67a186b370ca930be5e358a5dad65a5490361278
Saken Alimbayev <sakenalimbayev@gmail.com>
2019-03-28 15:47:32 +0600

    3 task fix

--- App/webpack.common.js
+++ App/webpack.common.js
@@ -1,19 +1,23 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-   entry: {
-     app: './index.js'
-   },
-   plugins: [
-     new CleanWebpackPlugin(['dist']),
-     new HtmlWebpackPlugin({
-       title: 'Production'
-     })
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist')
-   }
- };
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
